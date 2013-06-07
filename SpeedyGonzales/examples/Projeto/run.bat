ad20e1beee94b17c40b123d4927c45c9b1886fe3
rodrigodellinghausen <dellinghausen@gmail.com>
2013-06-06 22:39:36 -0300

    Projeto teste.

---
+++ SpeedyGonzales/examples/Projeto/run.bat
@@ -0,0 +1,4 @@
+echo "Inicio do processamento"
+java -cp "libs/SpeedyGonzales-1.0-SNAPSHOT.jar;libs/velocity-1.7.jar;libs/commons-lang-2.4.jar;libs/commons-collections-3.2.1.jar" com.rbs.speedygonzales.MainDefault "C:\Projetos\Testes SpeedyGonzales\project-config.xml"
+
+echo "FIM"
